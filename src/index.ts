ac24524adf8ecc13fc76cd6ead9c1da2b93ac9be
imdhemy <imdhemy@gmail.com>
2024-12-29 15:29:06 +0100

    wip: register commands

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import { createApp } from './command/create-app.command';
+import { Command as Program } from 'commander';
+import { readdirSync } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as process from 'node:process';
+import meta from '../package.json';
+import { IProgram } from './types';
 
-const program = new Command();
+const program: IProgram = new Program();
 
 program
     .name('KoalaTs CLI')
     .description('KoalaTs CLI is a command line interface for KoalaTs')
-    .version('1.0.0');
+    .version(meta.version);
 
-program
-    .command('create <name>')
-    .description('Create a new KoalaTs app')
-    .action(createApp);
+const rootDir = dirname(fileURLToPath(import.meta.url));
+const commandFiles: string[] = readdirSync(`${rootDir}/command`);
+const commands = commandFiles.filter(file => file.endsWith('.command.js') || file.endsWith('.command.ts'));
 
+for (const command of commands) {
+    const commandModule = await import(`./command/${command}`);
+    const commandFn = commandModule.default;
+    commandFn(program);
+}
 
 program.parse(process.argv);
--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
                 '**/playground/**',
                 '**/tests/**',
                 '**/types.ts',
+                '**/stub/**',
             ],
         },
     },
