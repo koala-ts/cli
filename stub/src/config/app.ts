9aaf2cac8a8c414c295a668e988856842e6b65c7
imdhemy <imdhemy@gmail.com>
2024-12-30 13:43:15 +0100

    Update app.ts

--- stub/src/config/app.ts
+++ stub/src/config/app.ts
@@ -1,5 +1,5 @@
 import { HomeController } from '../controller/HomeController';
-import { IKoalaConfig } from '@koala-ts/framework';
+import { type IKoalaConfig } from '@koala-ts/framework';
 
 export const appConfig: IKoalaConfig = {
     controllers: [
