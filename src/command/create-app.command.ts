c3423a12aa0fd7fa32658847607d6521b3461ba7
Dhemy <imdhemy@gmail.com>
2025-04-21 00:07:48 +0200

    chore(command): replace clone with download and extract
    - create app command

--- src/command/create-app.command.ts
+++ src/command/create-app.command.ts
@@ -1,19 +1,22 @@
-import simpleGit from 'simple-git';
 import logger from '../util/logger';
+import { download } from '../util/download';
+import { extractTar } from '../util/extract';
+import { mkdir } from 'node:fs/promises';
 
 export const signature = 'create <name>';
 export const description = 'Create a new KoalaTs app';
 
 export async function action(name: string) {
-  const git = simpleGit();
+    try {
+        await mkdir(name, { recursive: true });
+        const tar = await download('https://codeload.github.com/koala-ts/koala-ts/tar.gz/1.x');
+        await extractTar(tar, `./${name}`);
 
-  try {
-    await git.clone('https://github.com/koala-ts/koala-ts.git', `./${name}`);
-    logger.info(`✨ Your app has been successfully created in: ./${name}`);
-    logger.info(`📂 Navigate to the project directory: "cd ${name}"`);
-    logger.warn(`📦 Install dependencies: "npm install"`);
-    logger.warn(`🚀 Start the application: "npm start"`);
-  } catch (error) {
-    logger.error('Failed to clone the repository:', error);
-  }
+        logger.info(`✨ Your app has been successfully created in: ./${name}`);
+        logger.info(`📂 Navigate to the project directory: "cd ${name}"`);
+        logger.warn(`📦 Install dependencies: "npm install"`);
+        logger.warn(`🚀 Start the application: "npm start"`);
+    } catch (error) {
+        logger.error('Error creating app:', error);
+    }
 }
