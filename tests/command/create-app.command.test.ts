bc77e8b587c39863a64b8ea0f8ad0347cde0cdcb
Dhemy <imdhemy@gmail.com>
2025-04-21 15:05:24 +0200

    chore: improve tests

--- tests/command/create-app.command.test.ts
+++ tests/command/create-app.command.test.ts
@@ -2,18 +2,11 @@ import { describe, expect, test, vi } from 'vitest';
 import { action, description, signature } from '../../src/command/create-app.command';
 import { mkdir } from 'node:fs/promises';
 import { download, extractTar } from '../../src/util';
+import { Readable } from 'node:stream';
 
-vi.mock('node:fs/promises', () => ({
-    mkdir: vi.fn(),
-}));
-
-vi.mock('../../src/util/download', () => ({
-    download: vi.fn().mockResolvedValue('mock-tarball'),
-}));
-
-vi.mock('../../src/util/extract', () => ({
-    extractTar: vi.fn(),
-}));
+vi.mock('node:fs/promises');
+vi.mock('../../src/util/download');
+vi.mock('../../src/util/extract');
 
 describe('Create App Command', function () {
     test('Command signature', function () {
@@ -25,10 +18,23 @@ describe('Create App Command', function () {
     });
 
     test('command action', async function () {
+        const downloaded = new Readable();
+        vi.mocked(download).mockResolvedValue(downloaded);
+
         await action('test-app');
 
         expect(mkdir).toHaveBeenCalledWith('test-app', { recursive: true });
         expect(download).toHaveBeenCalledWith('https://codeload.github.com/koala-ts/koala-ts/tar.gz/1.x');
-        expect(extractTar).toHaveBeenCalledWith('mock-tarball', './test-app');
+        expect(extractTar).toHaveBeenCalledWith(downloaded, './test-app');
+    });
+
+    test('failure action', async function () {
+        vi.mocked(mkdir).mockRejectedValue(new Error('Failed to create directory'));
+
+        await action('test-app');
+
+        expect(mkdir).toHaveBeenCalledWith('test-app', { recursive: true });
+        expect(download).not.toHaveBeenCalled();
+        expect(extractTar).not.toHaveBeenCalled();
     });
 });
