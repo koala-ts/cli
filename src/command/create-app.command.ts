f602b5adcadb676d71cb625f696a539f23c1a766
Dhemy <imdhemy@gmail.com>
2025-10-21 14:28:08 +0200

    fix styling

--- src/command/create-app.command.ts
+++ src/command/create-app.command.ts
@@ -5,18 +5,18 @@ export const signature = 'create <name>';
 export const description = 'Create a new KoalaTs app';
 
 export async function action(name: string) {
-  try {
-    await mkdir(name, {recursive: true});
-    const tar = await download(
-      'https://codeload.github.com/koala-ts/koala-ts/tar.gz/2.x',
-    );
-    await extractTar(tar, `./${name}`);
+	try {
+		await mkdir(name, { recursive: true });
+		const tar = await download(
+			'https://codeload.github.com/koala-ts/koala-ts/tar.gz/2.x',
+		);
+		await extractTar(tar, `./${name}`);
 
-    logger.info(`✨ Your app has been successfully created in: ./${name}`);
-    logger.info(`📂 Navigate to the project directory: "cd ${name}"`);
-    logger.warn(`📦 Install dependencies: "npm install"`);
-    logger.warn(`🚀 Start the application: "npm start"`);
-  } catch (error) {
-    logger.error('Error creating app:', error);
-  }
+		logger.info(`✨ Your app has been successfully created in: ./${name}`);
+		logger.info(`📂 Navigate to the project directory: "cd ${name}"`);
+		logger.warn(`📦 Install dependencies: "npm install"`);
+		logger.warn(`🚀 Start the application: "npm start"`);
+	} catch (error) {
+		logger.error('Error creating app:', error);
+	}
 }
